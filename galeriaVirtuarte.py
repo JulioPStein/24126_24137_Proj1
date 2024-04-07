7f5f00ad81613299f3e18e94601bb42c6e03a0d3
Eduardo Tribst <eduardo.tribst@gmail.com>
2024-04-07 18:58:10 -0300

    Mudancas no main  e galeria virtuante
    criada a função cadastro no main responsável por gravar as informaçãoes obtidas pelo usuario sobre a obra a ser adicionada assim como sua adição em um arquivo texto.
    Para isso também foram criados as propriedades e desenvolvidas as funções gravar campos no arquivo e preencher campos

--- galeriaVirtuarte.py
+++ galeriaVirtuarte.py
@@ -1,22 +1,44 @@
-class Obras:
+class Obra:
     def __init__(self, nomeArq : str, paraGravacao : bool):
         self.AnoDaObra = ""
         self.MesDaObra = ""
         self.AutorDaObra = ""
         self.NomeDaObra = ""
+        self.Estilo = ""
         self.ValorEstimado = 0.0
         self.urlFoto = ""
         self._arquivo = nomeArq
         self._abertoParaGravacao = paraGravacao
 
+    @property
+    def PodeGravar(self):
+        return self._abertoParaGravacao
+    
+    @property
+    def Arquivo(self):
+        return self._arquivo
+
     def lerCamposDoArquivo(self):
         pass
 
     def gravarCamposNoArquivo(self):
-        pass
+        if self._abertoParaGravacao:
+            arquivo = open(self._arquivo, "a")
+            self.linha = self.AnoDaObra.ljust(4, " ") + "   " + self. MesDaObra.ljust(2, " ") + "   " + self.Estilo.ljust(15, " ") + "   " + self.NomeDaObra.ljust(20, " ") + "   " + self.AutorDaObra.ljust(20, " ") + "   " + self.ValorEstimado + "   " + self.urlFoto
+            arquivo.write(f"\n{self.linha}")
+
 
     def preencherCampos(self, novoAno, novoMes, novoAutor, novoNome, novoEstilo, novoValor, novaURL : str):
-        pass
+        if self._abertoParaGravacao:
+            self.AnoDaObra = novoAno
+            self.MesDaObra = novoMes
+            self.AutorDaObra = novoAutor
+            self.NomeDaObra = novoNome
+            self.Estilo = novoEstilo
+            self.ValorEstimado = novoValor
+            self.urlFoto = novaURL
+
+
 
     def fecharArquivo(self):
         pass
--- main.py
+++ main.py
@@ -16,7 +16,51 @@ def SeletorDeOpcoes():
         os.system("cls") or None
         AcoesPossiveis()
         acao = input("\nDigite o número referente à ação desejada:")
+        match acao:
+            case "0" : pass
+            case "1" : Cadastro() 
+            case "2" : pass
+            case "3" : pass
+            case "4" : pass
+            case "" : pass #Qualquer coisa inválida para não voltar em loop sem explicar ao usuario?
+
+def Cadastro():
+    from tkinter import filedialog
+    import galeriaVirtuarte
+
+    tiposDeArquivos = (   #VER SE SÃO ESSES  OS TIPOS DESEJADOS
+        ("Arquivos de texto", "*.txt"),
+        ("Arquivos JSON", "*.json"),
+        ("Qualquer aqruivo", "*.*")
+    )
+
+    nomeDoArquivo = filedialog.askopenfilename(
+        title="Selecione o arquivo desejado",
+        initialdir= r"c:\temp",
+        multiple = False,
+        filetypes = tiposDeArquivos
+    )
+
+
+    print("Dados da obra")
+    novoAno = input("Ano da conclusão da obra: ")
+    novoMes = input("Mês da conclusão da obra: ")
+    novoAutor = input("Autor da obra: ")
+    novoNome = input("Nome da obra: ")
+    novoEstilo = input("Estilo da obra: ")
+    novoValor = input("Valor da obra: ")
+    novaURL = input("Url da foto: ")
+
+    obra = galeriaVirtuarte.Obra(nomeDoArquivo, paraGravacao=True)
+
+    obra.preencherCampos(novoAno, novoMes, novoAutor, novoNome, novoEstilo, novoValor, novaURL)
+
+    obra.gravarCamposNoArquivo()
+
+    
+
 
 
 while __name__ == "__main__":
     SeletorDeOpcoes()
+
