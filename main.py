821f4636b40e679b261b7918c15aa385fe723cf7
Eduardo Tribst <eduardo.tribst@gmail.com>
2024-04-07 17:19:59 -0300

    Seletor de opções criado
    Um seletor repetitivo de opções foi adicionado ao codigo

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
 #arquivo insano
-#ve se da para ver meu texto
+import os
+
+def AcoesPossiveis():
+    print("\n\nCadastro de obras de arte .........1")
+    print("Listagem de obras de arte .........2")
+    print("Pagina web com obras de arte ......3")
+    print("Triângulo de Pascal ...............4")
+    print("Terminar a execução do programa ...0")
+
+
+def SeletorDeOpcoes():
+    
+    acao = "-"
+    while acao != "0":
+        os.system("cls") or None
+        AcoesPossiveis()
+        acao = input("\nDigite o número referente à ação desejada:")
+
+
+while __name__ == "__main__":
+    SeletorDeOpcoes()
